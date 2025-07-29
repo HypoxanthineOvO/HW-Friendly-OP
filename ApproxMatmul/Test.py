3681b659c3aabe95b718493d4848a8dc1f9de7e6
HypoxanthineOvO <hyx021203@163.com>
2025-07-29 20:46:17 +0800

    For JY Test

--- ApproxMatmul/Test.py
+++ ApproxMatmul/Test.py
@@ -7,9 +7,9 @@ import VSP
 if __name__ == "__main__":
     torch.manual_seed(42)
     ########## 1. Linear ##########
-    N = 30
-    IN_FEATURES = 10
-    OUT_FEATURES = 5
+    N = 1
+    IN_FEATURES = 3
+    OUT_FEATURES = 4
     BIAS = True
     linear_torch = nn.Linear(
         in_features = IN_FEATURES, out_features = OUT_FEATURES,
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     linear_mine = VSP.Linear(
         in_features = IN_FEATURES, out_features = OUT_FEATURES,
         bias = BIAS,
-        max_iter = 0.8
+        max_iter = 1.0,
     )
     linear_mine.load_torch_state_dict(linear_torch.state_dict())
     
@@ -33,3 +33,14 @@ if __name__ == "__main__":
     err = torch.nn.functional.mse_loss(output_torch, output_mine)
     print(f"Error: {err.item():.4f}")
     VSP.check( torch.allclose(output_torch, output_mine), "Same Output")
+    
+    ########## 2. Attention ##########
+    BATCH_SIZE = 2
+    SEQ_LEN = 4
+    EMBED_DIM = 8
+    NUM_HEADS = 2
+    approx = True
+    
+    attention_torch = nn.MultiheadAttention(
+        embed_dim=EMBED_DIM, num_heads=NUM_HEADS, batch_first=True
+    )
