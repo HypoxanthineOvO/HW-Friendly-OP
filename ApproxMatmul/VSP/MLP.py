046c8d285659013180a7218dcf2f60094feaa0f9
jy <1773483845@qq.com>
2025-09-21 16:12:47 +0800

    add<MLP.py>:把block添加了进去

--- ApproxMatmul/VSP/MLP.py
+++ ApproxMatmul/VSP/MLP.py
@@ -24,14 +24,18 @@ class Linear(torch.nn.Module):
         
         # Parse Approx_Config
         method = Approx_Config.get("Method", "A3")
-        if (method == "A3" or method == "Row"):
+        if (method == "A3" or method == "Row" or method == "Block"):
             max_iter = Approx_Config.get("Max_Iter", 1.0)
+            block_size = Approx_Config.get("Block_Size", 3)
             debug = Approx_Config.get("Debug", False)
             Final_Approx_Config = {
                 "Method": method,
                 "Max_Iter": max_iter,
                 "Debug": debug
             }
+
+            if method == "Block":
+                Final_Approx_Config["Block_Size"] = block_size
             # Check Validity
             if not (isinstance(max_iter, int) or isinstance(max_iter, float)):
                 raise TypeError("max_iter must be an integer or a float")
@@ -45,6 +49,8 @@ class Linear(torch.nn.Module):
                 if (method == "A3"):
                     int_iter = int(in_features * out_features // 2 * max_iter)
                 elif (method == "Row"):
+                    int_iter = int(in_features / 2 * max_iter)
+                elif (method == "Block"):
                     int_iter = int(in_features / 2 * max_iter)
                 Final_Approx_Config["Max_Iter"] = int_iter
             self.Approx_Config = Final_Approx_Config
